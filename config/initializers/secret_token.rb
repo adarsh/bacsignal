4a6493b422dc0619f7e4356e0be18492672d6010
Adarsh Pandit <adarshpandit@gmail.com>
2012-03-05 16:46:05 -0500

    Initial commit - suspended project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bacsignal::Application.config.secret_token = 'b2bef27d94e4fc5496c1b6ff22be9890e1183b4812a34025594280e184ab243bfb2c6a4c667275fa94e034b1c416ea00f6c7c7ae46e52fd2ecba52d4b0ab0502'
