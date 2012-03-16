f41fb15ea5ce32694a2025c7ebe4226dd5e8eb99
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-03-16 13:08:00 -0400

    Namespace main.js.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,25 @@
-function success(position) {
-  var latitude  = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  var mapUrl    = "http://maps.google.com/maps/api/staticmap?center=" +
-    latitude + "," + longitude +
-    "&zoom=14&size=320x160&sensor=false"
-  $('#map').attr('src', mapUrl).show();
-  console.log(position.coords.latitude + " " + position.coords.longitude);
-}
-function failure(failureMessage) {
-  $('#geolocation-failure').show();
-}
-if (geo_position_js.init()) {
-  geo_position_js.getCurrentPosition(
-          success, failure,
-              { enableHighAccuracy : true }
-                );
-}
+var Bacsignal = Bacsignal || {};
+
+var Bacsignal.Geo = {
+  success: function(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var mapUrl    = "http://maps.google.com/maps/api/staticmap?center=" +
+      latitude + "," + longitude +
+      "&zoom=14&size=320x160&sensor=false";
+    $('#map').attr('src', mapUrl).show();
+    console.log(position.coords.latitude + " " + position.coords.longitude);
+  },
+
+  failure: function(failureMessage) {
+    $('#geolocation-failure').show();
+  },
+
+  init: function() {
+    if (geo_position_js.init()) {
+      geo_position_js.getCurrentPosition(Bacsignal.Geo.success, Bacsignal.Geo.failure, { enableHighAccuracy: true });
+    }
+  }
+};
+
+Bacsignal.Geo.init();
