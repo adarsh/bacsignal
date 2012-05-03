e3a1547eaa0362ec6134363ba5da71735bae098e
Adarsh Pandit <adarshpandit@gmail.com>
2012-05-03 17:28:21 -0400

    Results now show closest location

--- app/assets/javascripts/geo.js
+++ app/assets/javascripts/geo.js
@@ -12,21 +12,17 @@ Bacsignal.Geo = {
 
 
     function dishesIndex(event, data){
-      _.each(data,
-          function(dish) {
-            var miles = distance(latitude, longitude,
-              dish.locations[0].lat, dish.locations[0].lng)
+      _.each(data, function(dish) {
+        var miles = locationFinder(dish.locations, latitude, longitude);
 
-            $("ul#dishes").append("<li>" +
-              dish.title +
-              " at " +
-              "<b>" + dish.restaurant_name + "</b>" +
-              " " + miles + " miles away" +
-              "</li>")
-          })
+        $("ul#dishes").append("<li>" +
+          dish.title +
+          " at " +
+          "<b>" + dish.restaurant_name + "</b>" +
+          " " + miles + " miles away" +
+          "</li>")
+      })
     }
-
-
   },
 
   failure: function(failureMessage) {
@@ -42,9 +38,23 @@ Bacsignal.Geo = {
 
 Bacsignal.Geo.init();
 
-
-function distance(lat1, lng1, lat2, lng2) {
+function distance (lat1, lng1, lat2, lng2) {
   return (3958 * 3.1415926 * Math.sqrt((lat2 - lat1) * (lat2 - lat1) +
         Math.cos(lat2 / 57.29578) * Math.cos(lat1 / 57.29578) *
         (lng2 - lng1) * (lng2 - lng1)) / 180).toFixed(2);
+};
+
+function locationFinder (locations, myLat, myLng) {
+  var miles = distance (myLat, myLng, locations[0].lat, locations[0].lng)
+
+  _.each(locations, function (place) {
+    var place_distance = distance (myLat, myLng, place['lat'], place['lng'])
+
+    if (place_distance < miles)
+    {
+      miles = place_distance;
+    }
+  });
+
+  return miles
 };
